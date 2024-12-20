874f9eb6ff52deb4e73c1435da8164db1c73c43f
AxlEnr <axel.garcia0814@gmail.com>
2024-12-20 00:54:57 -0600

    Create second modal with ContApp

--- src/js/bubble.js
+++ src/js/bubble.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', () => {
     createBubble();
     closeBubble();
+    createBubbleContapp();
+    closeBubbleContapp();
 });
 
 
@@ -16,15 +18,38 @@ function createBubble(){
 }
 
 function closeBubble() {
-   const closeBubbleBtn = document.querySelector('#closeBubble');
+   const closeBubbleBtn = document.querySelector('#closeBubbleSpoti');
    closeBubbleBtn.addEventListener('click', () => {
         const openBubble = document.querySelector('#bubbleSpoti');
         openBubble.style.display = 'none'
    }); 
    
-   const closeBubbleBtn2 = document.querySelector('#goTo');
+   const closeBubbleBtn2 = document.querySelector('#goToSpoti');
    closeBubbleBtn2.addEventListener('click', () => {
     const openBubble = document.querySelector('#bubbleSpoti');
+    openBubble.style.display = 'none'
+})
+}
+
+function createBubbleContapp(){
+    const bubbleSpoti = document.querySelector('#bubbleBtnContapp');
+    bubbleSpoti.addEventListener('click', (e) => {
+        e.preventDefault();
+        const openBubble = document.querySelector('#bubbleContapp');
+        openBubble.style.display = 'block';
+    });
+}
+
+function closeBubbleContapp() {
+   const closeBubbleBtn = document.querySelector('#closeBubbleContapp');
+   closeBubbleBtn.addEventListener('click', () => {
+        const openBubble = document.querySelector('#bubbleContapp');
+        openBubble.style.display = 'none'
+   }); 
+   
+   const closeBubbleBtn2 = document.querySelector('#goTo');
+   closeBubbleBtn2.addEventListener('click', () => {
+    const openBubble = document.querySelector('#bubbleContapp');
     openBubble.style.display = 'none'
 })
 }
